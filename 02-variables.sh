e1da64631a326fa462ee24548e08b2529ac688c0
akkimahesh <maheshakki799@gmail.com>
2026-04-09 13:05:32 +0530

    main

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1="Mahesh"
 PERSON2="Suresh"
 
-echo "PERSON1: Hello $PERSON2 How are you?"
-echo "PERSON2: Hello $PERSON1 I am fine. How are you?"
-echo "PERSON1: I am also fine. Thanks for asking."  
-echo "PERSON2: What About your family? $PERSON1"
+echo "$PERSON1: Hello $PERSON2 How are you?"
+echo "$PERSON2: Hello $PERSON1 I am fine. How are you?"
+echo "$PERSON1: I am also fine. Thanks for asking."  
+echo "$PERSON2: What About your family? $PERSON1"
