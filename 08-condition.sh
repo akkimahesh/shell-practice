f5e2e38772a59f752ff172e06c1298deab44632f
akkimahesh <maheshakki799@gmail.com>
2026-04-12 14:18:27 +0530

    main

--- 08-condition.sh
+++ 08-condition.sh
@@ -11,3 +11,12 @@ if [ $Number -gt 0 ]; then
 else
     echo "No $Number is not greater than 0"
 fi
+
+#if you want to check multiple conditions, you can use elif
+if [ $Number -gt 10 ]; then 
+    echo "Yes $Number is greater than 10"
+elif [ $Number -eq 10 ]; then
+    echo "Yes $Number is equal to 10"
+else
+    echo "No $Number is not greater than 0"
+fi
